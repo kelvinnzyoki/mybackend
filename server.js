85632de0f1393c1b878e2e00a38bbd7577a94e97
kelvinnzyoki <kelvinnzyokimaitha@gmail.com>
2025-12-14 02:16:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const PORT = process.env.PORT || 5432;
 const express = require("express");
 const app = express();
 const helmet = require("helmet");
-const cors = require("cors");
+// Configuration to allow requests from your specific domain
+const corsOptions = {
+  origin: 'https://kelvinnzyoki.github.io', 
+  optionsSuccessStatus: 200 // For legacy browser support
+}
 
-
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 app.use(helmet());
 
 app.get("/", (req, res) => {
