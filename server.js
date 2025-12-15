f89c77371bc27159c518d44e7c8cf1b1a8821826
kelvinnzyoki <kelvinnzyokimaitha@gmail.com>
2025-12-15 21:17:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -131,6 +131,6 @@ app.post("/record", async (req, res) => {
 });
 
 /* -------------------- START SERVER -------------------- */
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
