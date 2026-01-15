4cc040046170b6fd19c4b7f9b04ab645cb9896df
kelvinnzyoki <kelvinnzyokimaitha@gmail.com>
2026-01-15 17:48:53 +0300

    Remove explicit host binding for app listener

--- server.js
+++ server.js
@@ -195,4 +195,4 @@ app.get("/leaderboard", async (_, res) => {
 });
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, "0.0.0.0", () => console.log(`🚀 Server on port ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server on port ${PORT}`));
