684c5830b8957d46587331c66346998f6b3c2617
kelvinnzyoki <kelvinnzyokimaitha@gmail.com>
2026-01-21 16:48:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -166,7 +166,7 @@ app.post("/send-code", async (req, res) => {
   try {
   await resend.emails.send({
     from: "onboarding@resend.dev",
-    to: email,
+    to: kelvinnzyokimaitha@gmail.com,
     subject: "Verification Code",
     text: `Your code is: ${code}`,
   });
